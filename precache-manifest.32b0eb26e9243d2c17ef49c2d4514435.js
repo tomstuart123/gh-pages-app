7f9f14182c8d25bb64a1398d63341a82b345b89e
Tommy <tomstuart123@hotmail.co.uk>
2019-11-28 10:51:19 -0500

    Updates

---
+++ precache-manifest.32b0eb26e9243d2c17ef49c2d4514435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5e2d9a5e3f40052f650b7e88e62976",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "0b22a58e158524abdca6",
+    "url": "/gh-pages-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f06dd627de0e3850c6fa",
+    "url": "/gh-pages-app/static/js/2.fdf37de1.chunk.js"
+  },
+  {
+    "revision": "0b22a58e158524abdca6",
+    "url": "/gh-pages-app/static/js/main.6c0809b0.chunk.js"
+  },
+  {
+    "revision": "2cb596bcb00809b477e3",
+    "url": "/gh-pages-app/static/js/runtime-main.2ed0f26c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-app/static/media/logo.25bf045c.svg"
+  }
+]);
